cdee236d45dd4e1b840d1e2f57d8a57ba12af4e4
Henrique Recidive <recidive@gmail.com>
2017-05-25 10:14:15 -0300

    Added React Router and added a page for each example.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 import logo from './logo.svg'
 
@@ -10,32 +15,56 @@ import Example02MyComponent from './example-02/MyComponent'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Functional React</h2>
+      <Router>
+        <div className="App">
+
+          <div className="App-header">
+            <Link to="/">
+              <img src={logo} className="App-logo" alt="logo" />
+            </Link>
+            <h2>Functional React</h2>
+          </div>
+
+          <Navigation />
+
+          <p className="App-intro">
+            Functional React Examples. To see the source check the src folder.
+          </p>
+
+          <hr />
+
+          <Route exact path="/" component={ HomePage } />
+          <Route path="/example-01" component={ Example01Page } />
+          <Route path="/example-02" component={ Example02Page } />
         </div>
-
-        <p className="App-intro">
-          Functional React Examples. To see the source check the src folder.
-        </p>
-
-        <hr />
-
-        <h1>Example 1</h1>
-        <p>A class based component (not functional).</p>
-        <Example01MyComponent title="Hello World" />
-
-        <hr />
-
-        <h1>Example 2</h1>
-        <p>A functional component.</p>
-        <Example02MyComponent title="Hello World" />
-
-        <hr />
-      </div>
+      </Router>
     )
   }
 }
 
 export default App
+
+const Navigation = () => (
+  <ul>
+    <li><Link to="/example-01">Example 1</Link></li>
+    <li><Link to="/example-02">Example 2</Link></li>
+  </ul>
+)
+
+const HomePage = () => <h1>Click the links to access the examples!</h1>
+
+const Example01Page = () => (
+  <div className="App-main">
+    <h1>Example 1</h1>
+    <p>A class based component (not functional).</p>
+    <Example01MyComponent title="Hello World!" />
+  </div>
+)
+
+const Example02Page = () => (
+  <div className="App-main">
+    <h1>Example 2</h1>
+    <p>A functional component.</p>
+    <Example02MyComponent title="Hello World 2!" />
+  </div>
+)
